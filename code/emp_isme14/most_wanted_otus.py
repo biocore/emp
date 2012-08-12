df979dccf7e58130e6e4cad3d7c5b608449c35d4
Jai Ram Rideout <jai.rideout@gmail.com>
2012-08-12 13:56:39 -0700

    Added more tests for the most wanted script, and decomposed the main function into smaller functions. Still needs more decomposition and a couple more tests.

--- code/emp_isme14/most_wanted_otus.py
+++ code/emp_isme14/most_wanted_otus.py
@@ -48,9 +48,114 @@ def generate_most_wanted_list(output_dir, otu_table_fps, rep_set_fp, gg_fp,
                     % output_dir)
 
     logger = WorkflowLogger(generate_log_fp(output_dir))
+    commands, blast_results_fp, rep_set_cands_failures_fp, \
+        master_otu_table_ms_fp = _get_most_wanted_filtering_commands(
+            output_dir, otu_table_fps,
+            rep_set_fp, gg_fp, nt_fp, min_abundance, max_abundance,
+            min_categories, max_gg_similarity, e_value, word_size,
+            jobs_to_start)
+
+    # Execute the commands, but keep the logger open because
+    # we're going to write additional status updates as we process the data.
+    command_handler(commands, status_update_callback, logger,
+                    close_logger_on_success=False)
     commands = []
 
+    # We'll sort the BLAST results by percent identity (ascending) and pick the
+    # top n.
+    logger.write("Reading in BLAST results, sorting by percent identity, "
+                 "and picking the top %d OTUs.\n\n" % top_n)
+    top_n_mw = _get_top_n_blast_results(open(blast_results_fp, 'U'), top_n)
+
+    # Read in our filtered down candidate seqs file and latest filtered and
+    # collapsed OTU table. We'll need to compute some stats on these to include
+    # in our report.
+    logger.write("Reading in filtered candidate sequences and latest filtered "
+                 "and collapsed OTU table.\n\n")
+    mw_seqs = _get_rep_set_lookup(open(rep_set_cands_failures_fp, 'U'))
+    master_otu_table_ms = parse_biom_table(open(master_otu_table_ms_fp, 'U'))
+
+    # Write results out to tsv and HTML table.
+    logger.write("Writing most wanted OTUs results to TSV and HTML "
+                 "tables.\n\n")
+    mw_tsv_f = open(join(output_dir,
+                    'top_%d_most_wanted_otus.txt' % top_n), 'w')
+    mw_html_f = open(join(output_dir,
+                    'top_%d_most_wanted_otus.html' % top_n), 'w')
+    tsv_header = 'OTU ID\tSequence\tGreengenes taxonomy\t' + \
+                 'NCBI nt closest match\tNCBI nt % identity'
+    mw_tsv_f.write(tsv_header + '\n')
+
+    tsv_header += '\tAbundance by %s' % mapping_category
+    html_header = ''
+    for col in tsv_header.split('\t'):
+        html_header += '<th>%s</th>' % col
+    mw_html_f.write('<table><tr>' + html_header + '</tr>')
+
+    for otu_id, subject_id, percent_identity in top_n_mw:
+        # Grab all necessary information to be included in our report.
+        seq = mw_seqs[otu_id]
+
+        # Splitting code taken from
+        # http://code.activestate.com/recipes/496784-split-string-into-n-
+        #   size-pieces/
+        split_seq = [seq[i:i+20] for i in range(0, len(seq), 20)]
+
+        tax = master_otu_table_ms.ObservationMetadata[
+            master_otu_table_ms.getObservationIndex(otu_id)]['taxonomy']
+        gb_id = subject_id.split('|')[3]
+        ncbi_link = 'http://www.ncbi.nlm.nih.gov/nuccore/%s' % gb_id
+
+        # Compute the abundance of each most wanted OTU in each sample
+        # grouping and create a pie chart to go in the HTML table.
+        samp_types = master_otu_table_ms.SampleIds
+        counts = master_otu_table_ms.observationData(otu_id)
+        if len(counts) != len(samp_types):
+            raise WorkflowError("The number of observation counts does not "
+                                "match the number of samples in the OTU "
+                                "table.")
+
+        # Piechart code modified from matplotlib example:
+        # http://matplotlib.sourceforge.net/examples/pylab_examples/
+        #   pie_demo.html
+        figure(figsize=(6,6))
+        ax = axes([0.1, 0.1, 0.8, 0.8])
+        # Will auto-normalize the counts.
+        pie(counts, labels=samp_types, autopct='%1.1f%%', shadow=True)
+
+        output_img_dir = join(output_dir, 'img')
+        try:
+            makedirs(output_img_dir)
+        except OSError:
+            # It already exists, which is okay since we already know we are in
+            # 'force' mode from above.
+            pass
+
+        # We need a relative path to the image.
+        pie_chart_fp = join('img', 'abundance_by_%s_%s.png' %
+                            (mapping_category, otu_id))
+        savefig(join(output_dir, pie_chart_fp))
+
+        mw_tsv_f.write('%s\t%s\t%s\t%s\t%s\n' %
+                       (otu_id, seq, tax, gb_id, percent_identity))
+
+        mw_html_f.write('<tr><td>%s</td><td>%s</td><td>%s</td>'
+                '<td><a href="%s" target="_blank">%s</a></td><td>%s</td><td>'
+                '<img src="%s" width="300" height="300" /></td></tr>' %
+                (otu_id, '\n'.join(split_seq), tax, ncbi_link, gb_id,
+                 percent_identity, pie_chart_fp))
+    mw_html_f.write('</table>')
+    mw_tsv_f.close()
+    mw_html_f.close()
+    logger.close()
+
+def _get_most_wanted_filtering_commands(output_dir, otu_table_fps, rep_set_fp,
+        gg_fp, nt_fp, mapping_fp, mapping_category, min_abundance,
+        max_abundance, min_categories, max_gg_similarity, e_value, word_size,
+        jobs_to_start):
+    commands = []
     otu_tables_to_merge = []
+
     for otu_table_fp in otu_table_fps:
         # First filter to keep only new (non-GG) OTUs.
         novel_otu_table_fp = join(output_dir, add_filename_suffix(otu_table_fp,
@@ -136,110 +241,27 @@ def generate_most_wanted_list(output_dir, otu_table_fps, rep_set_fp, gg_fp,
             (rep_set_cands_failures_fp, blast_output_dir, nt_fp, e_value,
              word_size, jobs_to_start))])
 
-    # Execute the commands we have so far, but keep the logger open because
-    # we're going to write additional status updates as we process the data.
-    command_handler(commands, status_update_callback, logger,
-                    close_logger_on_success=False)
-    commands = []
+    blast_results_fp = join(blast_output_dir,
+            splitext(basename(rep_set_cands_failures_fp))[0] +
+                              '_blast_out.txt')
 
-    # We'll sort the BLAST results by percent identity (ascending) and pick the
-    # top n.
-    logger.write("Reading in BLAST results, sorting by percent identity, "
-                 "and picking the top %d OTUs.\n\n" % top_n)
-    blast_results = open(join(blast_output_dir,
-        splitext(basename(rep_set_cands_failures_fp))[0] +
-                          '_blast_out.txt'), 'U')
-    top_n_mw = []
-    for line in blast_results:
-        # Skip headers.
+    return commands, blast_results_fp, rep_set_cands_failures_fp, \
+           master_otu_table_ms_fp
+
+def _get_top_n_blast_results(blast_results_f, top_n):
+    """blast_results should only contain a single hit per query sequence"""
+    result = []
+    for line in blast_results_f:
+        # Skip headers and comments.
         line = line.strip()
         if line and not line.startswith('#'):
             line = line.split('\t')
-            top_n_mw.append((line[0], line[1], float(line[2])))
-    top_n_mw = sorted(top_n_mw, key=itemgetter(2))[:top_n]
+            result.append((line[0], line[1], float(line[2])))
+    return sorted(result, key=itemgetter(2))[:top_n]
 
-    # Read in our filtered down candidate seqs file and latest filtered and
-    # collapsed OTU table. We'll need to compute some stats on these to include
-    # in our report.
-    logger.write("Reading in filtered candidate sequences and latest filtered "
-                 "and collapsed OTU table.\n\n")
-    mw_seqs = {}
-    for seq_id, seq in MinimalFastaParser(
-            open(rep_set_cands_failures_fp, 'U')):
+def _get_rep_set_lookup(rep_set_f):
+    result = {}
+    for seq_id, seq in MinimalFastaParser(rep_set_f):
         seq_id = seq_id.strip().split()[0]
-        mw_seqs[seq_id] = seq
-    master_otu_table_ms = parse_biom_table(open(master_otu_table_ms_fp, 'U'))
-
-    # Write results out to tsv and HTML table.
-    logger.write("Writing most wanted OTUs results to TSV and HTML "
-                 "tables.\n\n")
-    mw_tsv_f = open(join(output_dir,
-                    'top_%d_most_wanted_otus.txt' % top_n), 'w')
-    mw_html_f = open(join(output_dir,
-                    'top_%d_most_wanted_otus.html' % top_n), 'w')
-    tsv_header = 'OTU ID\tSequence\tGreengenes taxonomy\t' + \
-                 'NCBI nt closest match\tNCBI nt % identity'
-    mw_tsv_f.write(tsv_header + '\n')
-
-    tsv_header += '\tAbundance by %s' % mapping_category
-    html_header = ''
-    for col in tsv_header.split('\t'):
-        html_header += '<th>%s</th>' % col
-    mw_html_f.write('<table><tr>' + html_header + '</tr>')
-
-    for otu_id, subject_id, percent_identity in top_n_mw:
-        # Grab all necessary information to be included in our report.
-        seq = mw_seqs[otu_id]
-
-        # Splitting code taken from
-        # http://code.activestate.com/recipes/496784-split-string-into-n-
-        #   size-pieces/
-        split_seq = [seq[i:i+20] for i in range(0, len(seq), 20)]
-
-        tax = master_otu_table_ms.ObservationMetadata[
-            master_otu_table_ms.getObservationIndex(otu_id)]['taxonomy']
-        gb_id = subject_id.split('|')[3]
-        ncbi_link = 'http://www.ncbi.nlm.nih.gov/nuccore/%s' % gb_id
-
-        # Compute the abundance of each most wanted OTU in each sample
-        # grouping and create a pie chart to go in the HTML table.
-        samp_types = master_otu_table_ms.SampleIds
-        counts = master_otu_table_ms.observationData(otu_id)
-        if len(counts) != len(samp_types):
-            raise WorkflowError("The number of observation counts does not "
-                                "match the number of samples in the OTU "
-                                "table.")
-
-        # Piechart code modified from matplotlib example:
-        # http://matplotlib.sourceforge.net/examples/pylab_examples/
-        #   pie_demo.html
-        figure(figsize=(6,6))
-        ax = axes([0.1, 0.1, 0.8, 0.8])
-        # Will auto-normalize the counts.
-        pie(counts, labels=samp_types, autopct='%1.1f%%', shadow=True)
-
-        output_img_dir = join(output_dir, 'img')
-        try:
-            makedirs(output_img_dir)
-        except OSError:
-            # It already exists, which is okay since we already know we are in
-            # 'force' mode from above.
-            pass
-
-        # We need a relative path to the image.
-        pie_chart_fp = join('img', 'abundance_by_%s_%s.png' %
-                            (mapping_category, otu_id))
-        savefig(join(output_dir, pie_chart_fp))
-
-        mw_tsv_f.write('%s\t%s\t%s\t%s\t%s\n' %
-                       (otu_id, seq, tax, gb_id, percent_identity))
-
-        mw_html_f.write('<tr><td>%s</td><td>%s</td><td>%s</td>'
-                '<td><a href="%s" target="_blank">%s</a></td><td>%s</td><td>'
-                '<img src="%s" width="300" height="300" /></td></tr>' %
-                (otu_id, '\n'.join(split_seq), tax, ncbi_link, gb_id,
-                 percent_identity, pie_chart_fp))
-    mw_html_f.write('</table>')
-    mw_tsv_f.close()
-    mw_html_f.close()
-    logger.close()
+        result[seq_id] = seq
+    return result
--- code/tests/test_most_wanted_otus.py
+++ code/tests/test_most_wanted_otus.py
@@ -24,7 +24,8 @@ from qiime.util import get_qiime_temp_dir, get_tmp_filename
 from qiime.workflow import WorkflowError
 
 from emp_isme14.most_wanted_otus import (generate_most_wanted_list,
-                                         _generate_new_otus_stats)
+        _get_most_wanted_filtering_commands, _get_top_n_blast_results,
+        _get_rep_set_lookup)
 
 class MostWantedOtusTests(TestCase):
     """Tests for the most_wanted_otus.py module."""
@@ -63,6 +64,9 @@ class MostWantedOtusTests(TestCase):
 
         self.grouping_category = 'Environment'
         self.top_n = 100
+
+        self.blast_results_lines = blast_results.split('\n')
+        self.rep_set_lines = rep_set.split('\n')
 
     def tearDown(self):
         """Remove temporary files/dirs."""
@@ -111,6 +115,23 @@ class MostWantedOtusTests(TestCase):
 
         self.assertFloatEqual(obs, exp)
 
+    def test_get_most_wanted_filtering_commands(self):
+        obs = _get_most_wanted_filtering_commands('/foo', ['/a.biom',
+                '/b.biom', '/c.biom'], '/rs.fna', '/gg.fasta', '/nt',
+                '/map.txt', 'Env', 30, 100, 5, 0.70, 1e-4, 25, 55)
+        self.assertEqual(obs, exp_commands)
+
+    def test_get_top_n_blast_results(self):
+        exp = [('New.CleanUp.ReferenceOTU969', 'gi|16|emb|Z52700.1|', 90.0),
+                ('New.CleanUp.ReferenceOTU999', 'gi|7|emb|X51700.1|', 100.0),
+                ('New.CleanUp.ReferenceOTU972', 'gi|7|emb|T51700.1|', 100.0)]
+        obs = _get_top_n_blast_results(self.blast_results_lines, self.top_n)
+        self.assertFloatEqual(obs, exp)
+
+    def test_get_rep_set_lookup(self):
+        obs = _get_rep_set_lookup(self.rep_set_lines)
+        self.assertEqual(obs, exp_rep_set_lookup)
+
 
 otu_table = """
 {"rows": [{"id": "10115", "metadata": {"taxonomy": "foo;bar;baz"}}, {"id": "10113", "metadata": {"taxonomy": "foo;bar;baz"}}, {"id": "102506", "metadata": {"taxonomy": "foo;bar;baz"}}, {"id": "New.CleanUp.ReferenceOTU964", "metadata": {"taxonomy": "foo;bar;baz"}}, {"id": "New.CleanUp.ReferenceOTU969", "metadata": {"taxonomy": "foo;bar;baz"}}, {"id": "New.CleanUp.ReferenceOTU972", "metadata": {"taxonomy": "foo;bar;baz"}}, {"id": "New.CleanUp.ReferenceOTU999", "metadata": {"taxonomy": "foo;bar;bazz"}}], "format": "Biological Observation Matrix 1.0.0", "data": [[0, 0, 6.0], [1, 0, 18.0], [2, 0, 6.0], [3, 0, 3.0], [4, 0, 6.0], [5, 0, 35.0], [6, 0, 99.0], [0, 1, 0.0], [1, 1, 1.0], [2, 1, 5.0], [3, 1, 2.0], [4, 1, 8.0], [5, 1, 25.0], [6, 1, 0.0]], "columns": [{"id": "S1", "metadata": null}, {"id": "S2", "metadata": null}], "generated_by": "QIIME 1.5.0-dev, svn revision 3175", "matrix_type": "sparse", "shape": [7, 2], "format_url": "http://biom-format.org", "date": "2012-08-06T16:56:21.226213", "type": "OTU table", "id": null, "matrix_element_type": "float"}
@@ -151,12 +172,34 @@ S1	TTCCAGGCAGAT	GGACTACHVGGGTWTCTAAT	Env1	S1_Env1
 S2	TTCCAGGCAGAT	GGACTACHVGGGTWTCTAAT	Env2	S2_Env2
 """
 
+blast_results = """
+# BLASTN 2.2.22 [Sep-27-2009]
+# Query: New.CleanUp.ReferenceOTU999
+# Database: small_nt
+# Fields: Query id, Subject id, % identity, alignment length, mismatches, gap openings, q. start, q. end, s. start, s. end, e-value, bit score
+New.CleanUp.ReferenceOTU999	gi|7|emb|X51700.1|	100.00	11	0	0	92	102	367	357	0.25	22.3
+# BLASTN 2.2.22 [Sep-27-2009]
+# Query: New.CleanUp.ReferenceOTU972
+# Database: small_nt
+# Fields: Query id, Subject id, % identity, alignment length, mismatches, gap openings, q. start, q. end, s. start, s. end, e-value, bit score
+New.CleanUp.ReferenceOTU972	gi|7|emb|T51700.1|	100.00	11	0	0	92	102	367	357	0.25	22.3
+# BLASTN 2.2.22 [Sep-27-2009]
+# Query: New.CleanUp.ReferenceOTU969
+# Database: small_nt
+# Fields: Query id, Subject id, % identity, alignment length, mismatches, gap openings, q. start, q. end, s. start, s. end, e-value, bit score
+New.CleanUp.ReferenceOTU969	gi|16|emb|Z52700.1|	90.00	13	0	0	33	45	1604	1616	0.016	26.3
+"""
+
 exp_txt = """OTU ID	Sequence	Taxonomy	NCBI nr closest match
 New.CleanUp.ReferenceOTU972	ATACGGAGGGTGCAAGCGTTAATCGGAATTACTGGGCGTAAAGGGTGCGTAGGCGGATGTTTAAGTGGGATGTGAAATCCCCGGGCTTAACCTGGGGGCTGC	foo;bar;baz	http://foo.com
 New.CleanUp.ReferenceOTU969	ATACGTAGGTCCCGAGCGTTGTCCGGATTTACTGGGTGTAAAGGGAGCGTAGACGGCATGGCAAGTCTGAAGTGAAAACCCAGGGCTCAACCCTGGGACTGC	foo;bar;baz	http://foo.com
 New.CleanUp.ReferenceOTU964	ATACGGAGGATGCGAGCGTTATCCGGATTTATTGGGTTTAAAGGGTGCGTAGACGGCGAAGCAAGTCTGAAGTGAAAGCCCGGGGCTCAACCGCGGGACTGC	foo;bar;baz	http://foo.com
 New.CleanUp.ReferenceOTU999	ATACGGAGGGTGCAAGCGTTAATCGGAATTACTGGGCGTAAAGGGTGCGTAGGCGGATGTTTAAGTGGGATGTGAAATCCCCGGGCTTAACCTGGGGGCTGC	foo;bar;bazz	http://foo.com
 """
+
+exp_commands = ([[('Filtering out all GG reference OTUs', 'filter_otus_from_otu_table.py -i /a.biom -o /foo/a_novel.biom -e /gg.fasta')], [('Filtering out all OTUs that do not fall within the specified abundance threshold', 'filter_otus_from_otu_table.py -i /foo/a_novel.biom -o /foo/a_novel_min30_max100.biom -n 30 -x 100')], [('Collapsing OTU table by Env', 'summarize_otu_by_cat.py -c /foo/a_novel_min30_max100.biom -o /foo/a_novel_min30_max100_Env.biom -m Env -i /map.txt')], [('Filtering out all GG reference OTUs', 'filter_otus_from_otu_table.py -i /b.biom -o /foo/b_novel.biom -e /gg.fasta')], [('Filtering out all OTUs that do not fall within the specified abundance threshold', 'filter_otus_from_otu_table.py -i /foo/b_novel.biom -o /foo/b_novel_min30_max100.biom -n 30 -x 100')], [('Collapsing OTU table by Env', 'summarize_otu_by_cat.py -c /foo/b_novel_min30_max100.biom -o /foo/b_novel_min30_max100_Env.biom -m Env -i /map.txt')], [('Filtering out all GG reference OTUs', 'filter_otus_from_otu_table.py -i /c.biom -o /foo/c_novel.biom -e /gg.fasta')], [('Filtering out all OTUs that do not fall within the specified abundance threshold', 'filter_otus_from_otu_table.py -i /foo/c_novel.biom -o /foo/c_novel_min30_max100.biom -n 30 -x 100')], [('Collapsing OTU table by Env', 'summarize_otu_by_cat.py -c /foo/c_novel_min30_max100.biom -o /foo/c_novel_min30_max100_Env.biom -m Env -i /map.txt')], [('Merging collapsed OTU tables', 'merge_otu_tables.py -i /foo/a_novel_min30_max100_Env.biom,/foo/b_novel_min30_max100_Env.biom,/foo/c_novel_min30_max100_Env.biom -o /foo/master_otu_table_novel_min30_max100_Env.biom')], [('Filtering OTU table to include only OTUs that appear in at least 5 sample groups', 'filter_otus_from_otu_table.py -i /foo/master_otu_table_novel_min30_max100_Env.biom -o /foo/master_otu_table_novel_min30_max100_Env_ms5.biom -s 5')], [('Filtering representative set to include only the latest candidate OTUs', 'filter_fasta.py -f /rs.fna -o /foo/rs_candidates.fna -b /foo/master_otu_table_novel_min30_max100_Env_ms5.biom')], [("Running uclust to get list of sequences that don't hit the maximum GG similarity threshold", 'parallel_pick_otus_uclust_ref.py -i /foo/rs_candidates.fna -o /foo/most_wanted_candidates_gg.fasta_0.7 -r /gg.fasta -s 0.7 -O 55')], [('Filtering candidate sequences to only include uclust failures', 'filter_fasta.py -f /foo/rs_candidates.fna -s /foo/most_wanted_candidates_gg.fasta_0.7/rs_candidates_failures.txt -o /foo/rs_candidates_failures.fna')], [('BLASTing filtered candidate sequences against nt database', 'parallel_blast.py -i /foo/rs_candidates_failures.fna -o /foo/blast_output -r /nt -D -e 0.000100 -w 25 -O 55')]], '/foo/blast_output/rs_candidates_failures_blast_out.txt', '/foo/rs_candidates_failures.fna', '/foo/master_otu_table_novel_min30_max100_Env_ms5.biom')
+
+exp_rep_set_lookup = {'New.CleanUp.ReferenceOTU999': 'ATACGGAGGGTGCAAGCGTTAATCGGAATTACTGGGCGTAAAGGGTGCGTAGGCGGATGTTTAAGTGGGATGTGAAATCCCCGGGCTTAACCTGGGGGCTGC', '102506': 'ATACGTATGGTGCAAGCGTTATCCGGATTTACTGGGTGTAAAGGGAGCGCAGGCGGTACGGCAAGTCTGATGTGAAAGTCCGGGGCTCAACCCCGGTACTGCAAACGTAGGGTGCAAGCGTTGTCCGGAATTACTGGGTGTAAAGGGAGCGTAGACGGCTGTGCAAGTCTGAAGTGAAAGGCATGGGCTCAACCTGTGGACTGC', 'New.CleanUp.ReferenceOTU972': 'ATACGGAGGGTGCAAGCGTTAATCGGAATTACTGGGCGTAAAGGGTGCGTAGGCGGATGTTTAAGTGGGATGTGAAATCCCCGGGCTTAACCTGGGGGCTGC', 'New.CleanUp.ReferenceOTU969': 'ATACGTAGGTCCCGAGCGTTGTCCGGATTTACTGGGTGTAAAGGGAGCGTAGACGGCATGGCAAGTCTGAAGTGAAAACCCAGGGCTCAACCCTGGGACTGC', 'New.CleanUp.ReferenceOTU964': 'ATACGGAGGATGCGAGCGTTATCCGGATTTATTGGGTTTAAAGGGTGCGTAGACGGCGAAGCAAGTCTGAAGTGAAAGCCCGGGGCTCAACCGCGGGACTGC', '10115': 'ATACGGAGGGTGCAAGCGTTAATCGGAATTACTGGGCGTAAAGCGCACGCAGGCGGTTTGTTAAGTTTGATGTGAAATCCCCGGGCTTAACCTGGGAACTGC', '10113': 'ATACGGAGGGTGCAAGCGTTAATCGGAATTACTGGGCGTAAAGCGCACGCAGGCGGTCTGTTAAGTCAGATGTGAAATCCCCGGGCTCCACCTGGGCACTGC'}
 
 
 if __name__ == "__main__":
